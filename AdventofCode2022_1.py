27dd069447389089d62ed3a1340df6a459e83fcf
georgegburns <88833145+georgegburns@users.noreply.github.com>
2022-12-01 16:32:53 +0000

    Add files via upload
    Day 1 - Task 1 and 2

---
+++ AdventofCode2022_1.py
@@ -0,0 +1,37 @@
+
+#importing the data from advent of code
+with open('input.txt') as i:
+    task = i.read()
+    task = task.splitlines()
+
+# part 1: putting the data into a list of lists split by none values (false)
+result = [[]]
+for i in task:
+    if not i:
+        result.append([])
+    else:
+        result[-1].append(i)
+
+# iterating through the list of lists and comparing the sum of each, replacing with a new highest value if found
+calories = 0
+for li in result:
+    temp = 0
+    for i in li:
+        temp += int(i)
+        if temp > calories:
+            calories = temp
+            
+#print(calories)
+
+# part 2: creating a list of summed lists
+calories2 = []
+for li in result:
+    temp = 0
+    for i in li:
+        temp += int(i)
+        calories2.append(temp)
+# ordering the list by highest value to lowest and then summing the top 3  
+calories2.sort(reverse=True)
+top3 = sum(calories2[0:3])
+
+print(top3)
